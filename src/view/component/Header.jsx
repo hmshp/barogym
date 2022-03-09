bb3825cda85eb851bbaa705ff490e6852586c5d3
hmshp <hyemisonne@gmail.com>
2022-03-09 10:40:28 +0900

    Refactor: 부트스트랩으로 헤더 제작
    프레임워크 없이 만들었던 기존 헤더를 부트스트랩을 이용해서 다시 제작했습니다.

--- src/view/component/Header.jsx
+++ src/view/component/Header.jsx
@@ -1,51 +1,73 @@
-import React from 'react';
+import * as React from 'react';
+import { Container, Navbar, Offcanvas } from 'react-bootstrap';
 import styled from 'styled-components';
+import Navi from './Navi';
 
-const HeaderElement = styled.header`
-  background: rgb(254, 139, 121);
-  color: white;
-`
+const navbarStyles = {
+  backgroundColor: 'rgb(254, 139, 121)',
+  color: 'white'
+}
 
-const HeaderContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 95%;
-  margin: 0 auto;
-  /* border: 1px solid green; */
-  padding: 0.2em 0;
-`
-
-const SiteTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const H1 = styled.h1`
-  margin: 0;
-`
+const navbarBrandStyles = {
+  display: 'flex',
+  margin: '0 3em 0 0'
+}
 
 const Logo = styled.img`
   width: 50px;
   margin-right: 1em;
 `
 
+const H1 = styled.h1`
+  margin: 0;
+  display: flex;
+  align-items: center;
+  color: white;
+`
+
 
 const Header = () => {
+
+  const [isToggled, setIsToggled] = React.useState(false);
+
+  const toggle = () => {
+    setIsToggled(prevIsToggled => !prevIsToggled)
+  }
+
   return (
-    <HeaderElement>
-      <HeaderContainer>
-        <SiteTitle>
-          <Logo src="https://i.ibb.co/smK8dwT/Kakao-Talk-20220308-145706090.png" alt="바로짐 로고" border="0" />
+    <Navbar style={navbarStyles} expand="md" onToggle={toggle}>
+      <Container fluid>
+        <Navbar.Brand style={navbarBrandStyles} href="/">
+          <Logo
+              alt="바로짐 로고"
+              src="https://i.ibb.co/smK8dwT/Kakao-Talk-20220308-145706090.png"
+            />  
           <H1>BAROGYM</H1>
-        </SiteTitle>
-        <div>
-          <i className="fas fa-bars" aria-label="네비게이션 토글 아이콘"></i>
-        </div>
-      </HeaderContainer>
-    </HeaderElement>
+        </Navbar.Brand>
+        <Navbar.Toggle/>
+        {
+          !toggle ?
+            <>
+              <Navbar.Collapse>
+                <Navi/>
+              </Navbar.Collapse>
+            </>
+          :
+            <>
+              <Navbar.Offcanvas
+                id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel"
+                placement="end"
+              >
+                <Offcanvas.Body>
+                  <Navi/>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </>
+        }
+      </Container>
+
+    </Navbar>
   );
 };
-
 export default Header;
