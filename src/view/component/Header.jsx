04d7971f24f62d50f08deea9f508ba59c4735c5a
hmshp <hyemisonne@gmail.com>
2022-03-10 17:06:47 +0900

    Revert "Fix: Navbar breakpoint 수정(md -> lg)"
    This reverts commit 37b22286e6e81b675614d8508c0ead63ab509f3c.

--- src/view/component/Header.jsx
+++ src/view/component/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
   }
 
   return (
-    <Navbar style={navbarStyles} expand="lg" onToggle={toggle}>
+    <Navbar style={navbarStyles} expand="md" onToggle={toggle}>
       <Container fluid>
         <Navbar.Brand style={navbarBrandStyles} href="/">
           <Logo
