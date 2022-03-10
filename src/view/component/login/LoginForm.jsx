bbee9f799429dff4a6258e4143f1ab67f40691db
hmshp <hyemisonne@gmail.com>
2022-03-10 21:52:46 +0900

    Design: 로그인 폼 레이아웃, 디자인 변경

--- src/view/component/login/LoginForm.jsx
+++ src/view/component/login/LoginForm.jsx
@@ -1,23 +1,55 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import styled from 'styled-components'
+
+const FORMCONTAINER = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const FORM = styled.form`
+  margin: 2em auto;
+  width: 80%;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+`
+
+const INPUT = styled.input`
+  height: 40px;
+  border-radius: 4px;
+  border: 1px solid gray;
+`
+
+const BUTTON = styled.button`
+  background-color: rgb(254, 139, 121);
+  color: white;
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  border: none;
+`
 
 export default function LoginForm() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   console.log(errors)
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <input
-        {...register("email", { required: '필수 입력사항입니다.' })}
-        placeholder="이메일을 입력해 주세요."
-      />
-      <p>{errors.email?.message}</p>
-      <input
-        {...register("password", { required: '필수 입력사항입니다.' })}
-        placeholder="비밀번호를 입력해 주세요."
-      />
-      <p>{errors.password?.message}</p>
-      <button>로그인</button>
-    </form>
+    <FORMCONTAINER>
+      <FORM onSubmit={handleSubmit((data) => console.log(data))}>
+        <INPUT
+          {...register("email", { required: '필수 입력사항입니다.' })}
+          placeholder="이메일을 입력해 주세요."
+        />
+        <p>{errors.email?.message}</p>
+        <INPUT
+          {...register("password", { required: '필수 입력사항입니다.' })}
+          placeholder="비밀번호를 입력해 주세요."
+        />
+        <p>{errors.password?.message}</p>
+        <BUTTON>로그인</BUTTON>
+      </FORM>
+    </FORMCONTAINER>
   );
 }
