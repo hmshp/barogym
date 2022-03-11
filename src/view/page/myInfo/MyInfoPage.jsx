b78277e0d42f732e74729286fabcf766083bf812
hmshp <hyemisonne@gmail.com>
2022-03-11 18:30:26 +0900

    Feat: isDisabled state 추가 회원정보 수정 버튼 클릭 시에만 input 수정 가능하도록 하였습니다.

--- src/view/page/myInfo/MyInfoPage.jsx
+++ src/view/page/myInfo/MyInfoPage.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import {useState, React} from 'react';
 import MyInfoModal from '../../component/myInfo/MyInfoModal';
+import MyContactInfo from '../../component/myInfo/MyContactInfo';
 
 const MyInfoPage = () => {
-  const [modalShow, setModalShow] = React.useState(true);
+  const [modalShow, setModalShow] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const activateInput = () => {
+    setIsDisabled(false)
+  }
 
   return (
     <div>
       <h1>내 정보</h1>
+      <MyContactInfo
+        isDisabled={isDisabled}
+        activateInput={activateInput}
+      />
       <MyInfoModal
         onHide={() => setModalShow(false)}
         show={modalShow}
