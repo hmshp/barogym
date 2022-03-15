f31ceacd859ce667641a28ed9c593a224485539c
hmshp <hyemisonne@gmail.com>
2022-03-15 18:14:20 +0900

    Feat: 비밀번호 변경 화면 제작

--- src/view/page/login/ResetPwdPage.jsx
+++ src/view/page/login/ResetPwdPage.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, {useState} from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+import {CONTAINER, FORM, FORMTITLE, FORMITEM, LABEL, INPUT, BUTTON, TWOBUTTONS, H1} from '../../../styles/MembershipStyle';
 
 const ResetPwdPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  console.log(errors)
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      비밀번호 변경 페이지
-    </div>
+    <CONTAINER>
+        <FORMTITLE>
+          <H1>비밀번호 변경</H1>
+        </FORMTITLE>
+        <FORM onSubmit={handleSubmit((data) => console.log(data))}>
+          <FORMITEM>
+            <LABEL htmlFor="name">이름</LABEL>
+            <INPUT
+              {...register("name")}
+              id="name"
+              required
+            />
+          </FORMITEM>
+          <FORMITEM>
+            <LABEL htmlFor="email">이메일</LABEL>
+            <INPUT
+              {...register("email")}
+              id="email"
+              required
+              type="email" //@ 안붙이면 안내 메시지 뜸
+            />
+          </FORMITEM>
+          <FORMITEM>
+            <LABEL htmlFor="phone">휴대폰</LABEL>
+            <INPUT
+              {...register("phone")}
+              id="phone"
+              required
+            />
+          </FORMITEM>
+          <TWOBUTTONS>
+            <BUTTON
+              onClick={()=>{ navigate('/login'); window.location.reload();}}
+              small gray
+            >
+              취소
+            </BUTTON>
+            <BUTTON small>확인</BUTTON>
+          </TWOBUTTONS>
+        </FORM>
+    </CONTAINER>
   );
 };
 
