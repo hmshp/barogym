a19fc4e967e79633b7b7238eacaf61ea58e01f5d
hmshp <hyemisonne@gmail.com>
2022-03-16 19:43:24 +0900

    Design: label, input 너비 속성 부여, 에러메시지 정렬 수정

--- src/view/page/login/ResetPwdPage.jsx
+++ src/view/page/login/ResetPwdPage.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
-import {CONTAINER, FORM, FORMTITLE, FORMITEM, LABEL, INPUT, BUTTON, TWOBUTTONS, H1} from '../../../styles/MembershipStyle';
+import {CONTAINER, FORM, FORMHEADER, FORMITEM, LABEL, INPUT, BUTTON, TWOBUTTONS, H1, ERRORMSG} from '../../../styles/MembershipStyle';
 
 const ResetPwdPage = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -11,43 +11,46 @@ const ResetPwdPage = () => {
 
   return (
     <CONTAINER>
-        <FORMTITLE>
+        <FORMHEADER>
           <H1>비밀번호 변경</H1>
-        </FORMTITLE>
+        </FORMHEADER>
         <FORM onSubmit={handleSubmit((data) => console.log(data))}>
-          <FORMITEM>
-            <LABEL htmlFor="name">이름</LABEL>
+          <div>
+            <LABEL thirty htmlFor="name">이름</LABEL>
             <INPUT
-              {...register("name")}
+              {...register("name", { required: '필수 입력사항입니다.' })}
               id="name"
-              required
+              seventy
             />
-          </FORMITEM>
-          <FORMITEM>
-            <LABEL htmlFor="email">이메일</LABEL>
+            <ERRORMSG rightAlign>{errors.name?.message}</ERRORMSG>
+          </div>
+          <div>
+            <LABEL thirty htmlFor="email">이메일</LABEL>
             <INPUT
-              {...register("email")}
+              {...register("email", { required: '필수 입력사항입니다.' })}
               id="email"
-              required
               type="email" //@ 안붙이면 안내 메시지 뜸
+              seventy
             />
-          </FORMITEM>
-          <FORMITEM>
-            <LABEL htmlFor="phone">휴대폰</LABEL>
+            <ERRORMSG rightAlign>{errors.email?.message}</ERRORMSG>
+          </div>
+          <div>
+            <LABEL thirty htmlFor="email">휴대폰</LABEL>
             <INPUT
-              {...register("phone")}
+              {...register("phone", { required: '필수 입력사항입니다.' })}
               id="phone"
-              required
+              seventy
             />
-          </FORMITEM>
+            <ERRORMSG rightAlign>{errors.phone?.message}</ERRORMSG>
+          </div>
           <TWOBUTTONS>
             <BUTTON
-              onClick={()=>{ navigate('/login'); window.location.reload();}}
-              small gray
+              onClick={()=>{ navigate('/login');}}
+              forty gray
             >
               취소
             </BUTTON>
-            <BUTTON small>확인</BUTTON>
+            <BUTTON forty>확인</BUTTON>
           </TWOBUTTONS>
         </FORM>
     </CONTAINER>
