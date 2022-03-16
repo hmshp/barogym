bcc4bc8a7f48518305a21969d548c872ae538fa0
hmshp <hyemisonne@gmail.com>
2022-03-16 20:56:22 +0900

    Style: 의미에 맞는 섹션 요소 사용

--- src/view/component/login/LoginForm.jsx
+++ src/view/component/login/LoginForm.jsx
@@ -7,18 +7,20 @@ export default function LoginForm() {
   console.log(errors)
 
   return (
-      <FORM onSubmit={handleSubmit((data) => console.log(data))}>
-        <INPUT
-          {...register("email", { required: '필수 입력사항입니다.' })}
-          placeholder="이메일을 입력해 주세요."
-        />
-        <ERRORMSG>{errors.email?.message}</ERRORMSG>
-        <INPUT
-          {...register("password", { required: '필수 입력사항입니다.' })}
-          placeholder="비밀번호를 입력해 주세요."
-        />
-        <ERRORMSG>{errors.password?.message}</ERRORMSG>
-        <BUTTON>로그인</BUTTON>
-      </FORM>
+      <main>
+        <FORM onSubmit={handleSubmit((data) => console.log(data))}>
+          <INPUT
+            {...register("email", { required: '필수 입력사항입니다.' })}
+            placeholder="이메일을 입력해 주세요."
+          />
+          <ERRORMSG>{errors.email?.message}</ERRORMSG>
+          <INPUT
+            {...register("password", { required: '필수 입력사항입니다.' })}
+            placeholder="비밀번호를 입력해 주세요."
+          />
+          <ERRORMSG>{errors.password?.message}</ERRORMSG>
+          <BUTTON>로그인</BUTTON>
+        </FORM>
+      </main>
   );
 }
