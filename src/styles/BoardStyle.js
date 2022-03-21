51f18da224d9025e7c0effc00cc296efe0679a12
hmshp <hyemisonne@gmail.com>
2022-03-21 19:23:15 +0900

    Design: 글 상세보기, 글쓰기 화면에서 필요한 스타일 추가

--- src/styles/BoardStyle.js
+++ src/styles/BoardStyle.js
@@ -7,6 +7,11 @@ export const PAGEHEADER = styled.header`
   padding-bottom: 1em;
   display: flex;
   justify-content: space-between;
+  ${props => props.center && css`
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+  `}
 `
 
 export const CONTAINER = styled.article`
@@ -40,6 +45,10 @@ export const H1 = styled.h1`
   color: rgb(254, 139, 121);
 `
 
+export const H2 = styled.h2`
+  color: rgb(254, 139, 121);
+`
+
 /* 글 목록 페이지 */
 export const PAGINATION = styled(Pagination)`
   display: flex;
@@ -63,7 +72,6 @@ export const BUTTON = styled.button`
     width: 30%;
   `}
   
-  
   @media(min-width: 800px) {
     ${props =>
     props.bottom &&
@@ -73,3 +81,79 @@ export const BUTTON = styled.button`
     `}
   }
 `
+
+/* 양도하기 버튼 */
+export const TRANSBUTTON = styled(BUTTON)`
+  width: 100%;
+  margin-bottom: 1em;
+`
+
+/* 글쓰기 페이지 */
+export const TWOBUTTONS = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1em;
+  ${props => props.flexEnd && css`
+    justify-content: flex-end;
+  `}
+`
+
+/* 글 상세보기 페이지 */
+export const GRIDCONTAINER = styled.article`
+  width: 80%;
+  font-size: 0.8rem;
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  @media(min-width: 800px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`
+
+export const FORM = styled.form`
+  /* display: grid;
+  row-gap: 20px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 2em;
+`
+
+export const TEXTAREA = styled.textarea`
+  width: 100%;
+`
+
+/* 글 상세보기 - 댓글 */
+
+/* 글쓰기 화면 폼 */
+export const INPUT = styled.input`
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  border: 1px solid gray;
+  ${props => props.seventy && css`
+    width: 70%;
+  `}
+  ${props => props.sixty && css`
+    width: 60%;
+  `}
+  ${props => props.full && css`
+    width: 100%;
+  `}
+`
+
+export const LABEL = styled.label`
+  width: ${props => props.pwCheck ? "100%" : props.thirty ? "30%" : "50%"};
+  margin-bottom: 1em;
+  font-weight: 700;
+  ${props => props.center && css`
+    justify-self: center;
+  `}
+`
+
+export const ERRORMSG = styled.p`
+  color: gray;
+  ${props => props.rightAlign && css`
+    text-align: right;
+  `}
+`
+
