8f20d8595c84657548d4f102a8d0c898c904c663
kimsarng <keemsarang@naver.com>
2022-03-24 21:38:09 +0900

    이용권, 예약, 홈화면, Footer 수정

--- src/view/page/intro/IntroGymPage.jsx
+++ src/view/page/intro/IntroGymPage.jsx
@@ -1,10 +1,29 @@
 import Carousel from 'react-bootstrap/Carousel'
 import "./style.css";
 import { H1 } from '../../../styles/MyInfoStyle';
+import styled, {css} from 'styled-components';
 
+export const STYLEDCONTAINER = styled.div`
+  margin: 0 auto;
+  @media(min-width: 800px) {
+    ${props =>
+    !props.pwCheck &&
+    css`
+      padding: 0 2em;
+    `}
+  }
+  width: 90%;
+  ${props =>
+  props.relative &&
+  css`
+    position: relative;
+  `}
+`
 const IntroGymPage = () => {
 
   return (
+    <STYLEDCONTAINER>
+      
     <div style={{ textAlign: "center" }}>
       <br />
       <H1>피트니스 소개</H1>
@@ -13,7 +32,7 @@ const IntroGymPage = () => {
       <Carousel>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="BAROGYM"
             src="https://images.unsplash.com/photo-1561214078-f3247647fc5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="First slide"
           />
@@ -24,11 +43,10 @@ const IntroGymPage = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="BAROGYM"
             src="https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="Second slide"
           />
-
           <Carousel.Caption>
             <h3>BAROGYM</h3>
             <p>피트니스를 위한 최상의 환경을 제공합니다.</p>
@@ -36,11 +54,10 @@ const IntroGymPage = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="BAROGYM"
             src="https://images.unsplash.com/photo-1596357395104-ba989e72b5ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
             alt="Third slide"
           />
-
           <Carousel.Caption>
             <h3>BAROGYM</h3>
             <p>첨단 헬스장비로 운동하는 피트니스 센터</p>
@@ -48,49 +65,42 @@ const IntroGymPage = () => {
         </Carousel.Item>
       </Carousel>
       <br />
-      <h4>
-        안녕하세요 송파구 최대규모의 피트니스 센터
+      <h5>
+        안녕하세요 송파구 최대규모의 피트니스
         <br />
         바로짐에 오신것을 진심으로 환영합니다.{" "}
-      </h4>
+      </h5>
       <br />
       <br />
 
-      <div className='intro'>
-        <p>
-          <img
-            className="introImg"
-            src="https://images.unsplash.com/photo-1596357395104-ba989e72b5ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-            alt="Third slide"
-            align="left"
-          />
-          안녕하세요 송파구 최대규모의 피트니스 센터 바로짐에 오신것을 진심으로
-          환영합니다. 바로짐은 최상의 환경을 제공하기 위해 첨단 헬스장비를
-          구비해두었습니다. 많이 찾아주십시오.
-        </p>
-        <br />
-        <br />
+      <p>
+        <img
+          className="introImg"
+          src="https://i.ibb.co/qntfN81/gym-gedeb5f16f-1920.jpg"
+          alt="1"
+        />
+      </p>
+      안녕하세요 송파구 최대규모의 피트니스 센터    <br />
+      바로짐에 오신것을 진심으로 환영합니다.    <br />
+      바로짐은 최상의 환경을 제공하기 위해    <br />
+      첨단 헬스장비를 구비해두었습니다.  <br />
 
-        <p>
-          <img
-            className="introImg"
-            src="https://images.unsplash.com/photo-1520877880798-5ee004e3f11e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-            alt="Third slide"
-            align="right"
-          />
-          안녕하세요 송파구 최대규모의 피트니스 센터 바로짐에 오신것을 진심으로
-          환영합니다. 바로짐은 최상의 환경을 제공하기 위해 첨단 헬스장비를
-          구비해두었습니다. 또한 다양한 GX프로그램도 준비해 두었습니다. 이제
-          바로짐 스포츠클럽에서 만나보세요! 환영합니다 반가워요
-        </p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <hr></hr>
-        <br />
+          <p>
+
+        <img
+          className="introImg"
+          src="https://i.ibb.co/3sNMVH5/trust-tru-katsande-A-fts-Th53l-M-unsplash.jpg"
+          alt="2"
+        />
+      </p>
+      안녕하세요 송파구 최대규모의 피트니스 센터    <br />
+      바로짐에 오신것을 진심으로 환영합니다.    <br />
+      바로짐은 최상의 환경을 제공하기 위해    <br />
+      첨단 헬스장비를 구비해두었습니다.  <br />
+
+
       </div>
-    </div>
+    </STYLEDCONTAINER>
   );
 };
 
