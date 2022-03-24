3f3960d7c451f5e1da21a27c3ac7b2902675eab3
hmshp <hyemisonne@gmail.com>
2022-03-24 17:40:04 +0900

    Feat: 글 클릭 시 상세보기 페이지로 이동

--- src/view/page/board/notice/NoticeListPage.jsx
+++ src/view/page/board/notice/NoticeListPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Table} from 'react-bootstrap';
 import {CONTAINER, PAGEHEADER, H1, BUTTON} from '../../../../styles/BoardStyle';
 import { useLocation } from 'react-router-dom';
@@ -8,17 +8,19 @@ import BoardPagination from '../../../component/board/BoardPagination';
 import BoardSearchBar from '../../../component/board/BoardSearchBar';
 import ReviewListFilter from '../../../component/board/classReview/ReviewListFilter';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useNavigate } from 'react-router-dom'
 
-const NoticeListPage = () => {
-
+const NoticeListPage = () => { 
   const listHeaders = BoardBody["notice"].listHeader
   const listItems = BoardBody["notice"].listBody
+
+  const navigate = useNavigate();
 
   const listHeadersElements = listHeaders.map((listHeader, index) => <th key={index}>{listHeader}</th>)
 
   const listItemsElements = listItems.map((listItem, index) => {
     return (
-      <tr key={index}>
+      <tr key={index} onClick={() => navigate(`/board/notice/detail?page=1&bno=${index}`)}>
         {Object.keys(listItem).map((key, index) => (
           <td key={index}>{listItem[key]}</td>
         )) }
--- src/view/page/board/trans/TransListPage.jsx
+++ src/view/page/board/trans/TransListPage.jsx
@@ -1,24 +1,25 @@
 import React, {useState} from 'react';
 import {Table} from 'react-bootstrap';
 import {CONTAINER, PAGEHEADER, H1, BUTTON} from '../../../../styles/BoardStyle';
-import { useLocation } from 'react-router-dom';
 import BoardBody from '../../../sampleData/BoardBody.json'
 import BoardHeader from '../../../sampleData/BoardHeader.json'
 import BoardPagination from '../../../component/board/BoardPagination';
 import BoardSearchBar from '../../../component/board/BoardSearchBar';
 import TransListFilter from '../../../component/board/trans/TransListFilter';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const TransListPage = () => {
-
   const listHeaders = BoardBody["trans"].listHeader
   const listItems = BoardBody["trans"].listBody
+
+  const navigate = useNavigate();
 
   const listHeadersElements = listHeaders.map((listHeader, index) => <th key={index}>{listHeader}</th>)
 
   const listItemsElements = listItems.map((listItem, index) => {
     return (
-      <tr key={index}>
+      <tr key={index} onClick={() => navigate(`/board/trans/detail?page=1&bno=${index}`)}>
         {Object.keys(listItem).map((key, index) => (
           <td key={index}>{listItem[key]}</td>
         )) }
