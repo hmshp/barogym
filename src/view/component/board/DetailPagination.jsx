a2ce4caa39862c204097158ba8d0e96e5e50e626
hmshp <hyemisonne@gmail.com>
2022-03-24 23:14:50 +0900

    Feat: '목록' 버튼에 링크 연결

--- src/view/component/board/DetailPagination.jsx
+++ src/view/component/board/DetailPagination.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Pagination }  from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useLocation } from 'react-router-dom';
 
 const DetailPagination = () => {
+  const boardName = `${useLocation().pathname.split('/')[1]}/${useLocation().pathname.split('/')[2]}/list?page=1`
+  console.log(boardName)
+
   return (
     <div>
       <Pagination size="lg" className="justify-content-center">
         <Pagination.Prev />
-        <Pagination.Item>목록으로</Pagination.Item>
+        <LinkContainer to={`/${boardName}`}>
+          <Pagination.Item>목록으로</Pagination.Item>
+        </LinkContainer>
+        
         <Pagination.Next />
       </Pagination>
     </div>
