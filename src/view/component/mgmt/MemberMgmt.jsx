0ccc734ec7ba944d409f495959cd122fd2c36af1
hmshp <hyemisonne@gmail.com>
2022-03-26 18:15:31 +0900

    Feat: 회원관리 표 항목 백엔드 테이블에 맞게 수정

--- src/view/component/mgmt/MemberMgmt.jsx
+++ src/view/component/mgmt/MemberMgmt.jsx
@@ -8,17 +8,35 @@ const MemberMgmt = () => {
     {
       name: "손혜미",
       phone: "010-1234-5678",
-      address: "서울시 강남구"
+      address: "서울시 강남구",
+      nickname: '별명',
+      email: 'abc@gmail.com',
+      tel: '010-1234-5678',
+      birthday: '3/25',
+      gender: '여성',
+      joinDate: '2022-03-24'
     },
     {
       name: "김사랑",
       phone: "010-1234-5678",
-      address: "서울시 강남구"
+      address: "서울시 강남구",
+      nickname: '별명',
+      email: 'abc@gmail.com',
+      tel: '010-1234-5678',
+      birthday: '3/25',
+      gender: '여성',
+      joinDate: '2022-03-24'
     },
     {
       name: "지승완",
       phone: "010-1234-5678",
-      address: "경기도 용인시"
+      address: "경기도 용인시",
+      nickname: '별명',
+      email: 'abc@gmail.com',
+      tel: '010-1234-5678',
+      birthday: '3/25',
+      gender: '남성',
+      joinDate: '2022-03-24'
     },
   ]
 
@@ -28,6 +46,12 @@ const MemberMgmt = () => {
         <td>{memberItem.name}</td>
         <td>{memberItem.phone}</td>
         <td>{memberItem.address}</td>
+        <td>{memberItem.nickname}</td>
+        <td>{memberItem.email}</td>
+        <td>{memberItem.tel}</td>
+        <td>{memberItem.birthday}</td>
+        <td>{memberItem.gender}</td>
+        <td>{memberItem.joinDate}</td>
         <td><BUTTON gray full>삭제</BUTTON></td>
       </tr>
     )
@@ -40,6 +64,13 @@ const MemberMgmt = () => {
           <th>이름</th>
           <th>전화번호</th>
           <th>주소</th>
+          <th>닉네임</th>
+          <th>이메일</th>
+          <th>전화번호</th>
+          <th>생일</th>
+          <th>성별</th>
+          <th>가입일</th>
+          <th>삭제</th>
         </tr>
       </thead>
       <tbody>
