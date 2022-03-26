3a21cd988884c9a10c1d66d04967f103017a3dc7
hmshp <hyemisonne@gmail.com>
2022-03-26 18:21:05 +0900

    Feat: 함수 추가(틀만 만들어놓았고 미완성임)

--- src/service/dbLogic.js
+++ src/service/dbLogic.js
@@ -19,3 +19,80 @@ export const memberListDB = (uid, type) => {
   });
 
 }
+
+export const boardListDB = (id, bno) => {
+
+  return new Promise((resolve) => {
+    const response = axios({
+      method: 'get',
+      url: process.env.REACT_APP_SPRING_IP+'board/list',
+      params: {
+
+      }
+    });
+    console.log(response);
+    resolve(response);
+  });
+
+}
+
+export const boardDetailDB = (id, bno) => {
+
+  return new Promise((resolve) => {
+    const response = axios({
+      method: 'get',
+      url: process.env.REACT_APP_SPRING_IP+'board/detail',
+      params: {
+
+      }
+    });
+    console.log(response);
+    resolve(response);
+  });
+}
+
+export const boardUpdateDB = (id, bno) => {
+
+  return new Promise((resolve) => {
+    const response = axios({
+      method: 'put',
+      url: process.env.REACT_APP_SPRING_IP+'board/detail',
+      params: {
+
+      }
+    });
+    console.log(response);
+    resolve(response);
+  });
+}
+
+export const boardDeleteDB = (id, bno) => {
+
+  return new Promise((resolve) => {
+    const response = axios({
+      method: 'delete',
+      url: process.env.REACT_APP_SPRING_IP+'board/detail',
+      params: {
+
+      }
+    });
+    console.log(response);
+    resolve(response);
+  });
+}
+
+export const uploadImageDB = (id, bno) => {
+
+  return new Promise((resolve) => {
+    const response = axios({
+      method: 'post',
+      // url: process.env.REACT_APP_SPRING_IP+'board/detail',
+      // params: {
+
+      // }
+    });
+    console.log(response);
+    resolve(response);
+  });
+}
+
