67dacfc9ede1d7bc57feed182ebf1e9e4fc13bd1
hmshp <hyemisonne@gmail.com>
2022-04-01 13:01:22 +0900

    Refactor: 우편번호, 주소 값을 state에서 관리

--- src/view/component/login/Zipcode.jsx
+++ src/view/component/login/Zipcode.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import { BUTTON, INPUT } from '../../../styles/MembershipStyle';
 
-const Zipcode = () => {
+const Zipcode = (props) => {
+  const { signUpFormData, setSignUpFormData, handleChange } = props;
 
   const openZipcode = () => {
     new daum.Postcode({
@@ -18,8 +19,13 @@ const Zipcode = () => {
           addr = data.jibunAddress;
         }
         // 우편번호와 주소 정보를 해당 필드에 넣는다.
-        document.getElementById('zipcode').value = data.zonecode;
-        document.getElementById("address").value = addr;
+        setSignUpFormData(prevFormData => {
+          return {
+            ...prevFormData,
+            mem_zipcode: data.zonecode,
+            mem_addr: addr
+          }
+        })
         // 커서를 상세주소 필드로 이동한다.
         document.getElementById("detailAddress").focus();
       }
@@ -29,12 +35,12 @@ const Zipcode = () => {
   return (
     <>
       <div>
-        <INPUT sixty type="text" id="zipcode" placeholder="우편번호"/>
+        <INPUT sixty type="text" id="zipcode" value={signUpFormData.mem_zipcode} onChange={handleChange} placeholder="우편번호"/>
         <BUTTON forty gray type="button" onClick={()=>{openZipcode()}}>우편번호 찾기</BUTTON>
           <br/>
-        <INPUT full type="text" id="address" placeholder="주소"/>
+        <INPUT full type="text" id="address" name="mem_addr" placeholder="주소" value={signUpFormData.mem_addr} onChange={handleChange}/>
           <br/>
-        <INPUT full type="text" id="detailAddress" placeholder="상세주소"/>
+        <INPUT full type="text" id="detailAddress" name="mem_addr_dtl" placeholder="상세주소" value={signUpFormData.mem_addr_dtl} onChange={handleChange}/>
       </div>
     </>
   );
