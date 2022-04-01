14f66178169d007aac1e788223da4da11394c4b7
hmshp <hyemisonne@gmail.com>
2022-04-01 13:04:15 +0900

    Feat: DB에서 상품 불러오기, 선택한 상품 state로 관리

--- src/view/page/pass/PassBuyPage.jsx
+++ src/view/page/pass/PassBuyPage.jsx
@@ -1,74 +1,58 @@
-import React from 'react';
-import {PAGEHEADER,STYLEDBUTTON,STYLEDCONTAINER} from '../../../styles/ClassStyle'
+import React, {useState, useEffect} from 'react';
+import {PAGEHEADER,STYLEDBUTTON,STYLEDCONTAINER, INPUT, LABEL} from '../../../styles/ClassStyle'
 import { Link } from 'react-router-dom' 
 import styled from 'styled-components';
 import { H1 } from '../../../styles/MyInfoStyle';
 import { themeColor } from '../../../styles/GlobalStyle';
-
-export const STYLEDBUTTON2 = styled.button`
-  background-color: ${(props) => (props.gray ? "gray" : themeColor)};
-  color: white;
-  padding: 0.5em 1em;
-  margin-bottom: 20px;
-  border-radius: 4px;
-  border: none;
-  @media (min-width: 800px) {
-  }
-`;
+//참고한 링크: https://medium.com/codex/handling-checkboxes-in-react-3a2514b140d2
+//https://stackoverflow.com/questions/57341541/removing-object-from-array-using-hooks-usestate
 
 const PassBuyPage = () => {
+  const [productList, setProductList] = useState([]);
+  const [selectedList, setSelectedList] = useState([]);//체크된 상품의 PROD_NO 넣음
+
+  console.log('목록: ' + selectedList)
+
+  const handleChange = (event) => {
+    const isChecked = event.target.checked;
+    if(isChecked) { //체크 시 목록에 추가
+      setSelectedList(prevList => [...prevList, event.target.name])
+    } else { //체크 해제 시 목록에서 삭제
+      const index = selectedList.indexOf(event.target.name)
+      const temp = [...selectedList]//state를 직접 바꾸면 안 되니 복사
+      temp.splice(index, 1);
+      setSelectedList(temp);
+    }
+  }
+
+  console.log(productList)
+
+  useEffect(() => {
+    fetch(`http://localhost:9000/request/productList`)
+      .then(res => res.json())
+      .then(result => setProductList(result))
+  }, [])
+  
+  const productListElements = productList.map(product => {
+    return (
+      <div key={product.PROD_NO}>
+        <INPUT type="checkbox" name={product.PROD_NO} id={product.PROD_NO} onChange={handleChange} />
+        <LABEL htmlFor={product.PROD_NO} seventy>{product.PROD_NAME}</LABEL>
+      </div>
+    )
+  })
+
   return (
     <div>
       <STYLEDCONTAINER>
-
-    <PAGEHEADER>
-          <H1>이용권 구매</H1>
+        <PAGEHEADER>
+          <H1>이용권 신청</H1>
         </PAGEHEADER>
-
-        <form name="orderform" id="orderform" method="post"
-          className="orderform" action="/Order" >
-
-          <div className="basket" id="basket">
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-            <strong> GX 패키지 5회 </strong>70,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-            <strong> GX 패키지 10회 </strong>140,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-              <strong> GX 패키지 20회 </strong>260,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-            <strong> GX 패키지 30회 </strong>380,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-            <strong> 헬스장 이용권 1개월 </strong>50,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-            <strong> 헬스장 이용권 3개월 </strong>140,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-              <strong> 헬스장 이용권 6개월 </strong>250,000원
-            </div>
-            <div className="PassPrice">
-            <input type="checkbox" className="checkbox" />
-            <strong> 헬스장 이용권 12개월 </strong>480,000원
-            </div>
-            </div>
-            <br/>
-            <h5><strong>총 합계 : 70,000원</strong></h5>
-            <br/>
-            <Link to="/pass/buyComplete">
-<STYLEDBUTTON2> 결제하기 </STYLEDBUTTON2>
-</Link>
+        <form>
+          {productListElements}
         </form>
+        <br />
+        <STYLEDBUTTON>신청하기</STYLEDBUTTON>
       </STYLEDCONTAINER>
     </div>
   );
