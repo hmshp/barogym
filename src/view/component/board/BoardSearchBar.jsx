faf39d6366e115d242eae072ed7b81a9c626b993
hmshp <hyemisonne@gmail.com>
2022-04-02 21:05:33 +0900

    Feat: 게시글 검색 기능 구현

--- src/view/component/board/BoardSearchBar.jsx
+++ src/view/component/board/BoardSearchBar.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import {Form, FormControl, Button} from 'react-bootstrap';
+//참고한 사이트: https://medium.com/how-to-react/create-pagination-in-reactjs-e4326c1b9855
 
-const BoardSearchBar = () => {
+const BoardSearchBar = (props) => {
+  const { searchText, setSearchText } = props;
+
+  const handleChange = (event) => {
+    setSearchText(event.target.value)
+  }
+
+
   return (
     <Form className="d-flex">
         <FormControl
@@ -9,8 +17,9 @@ const BoardSearchBar = () => {
           placeholder="제목을 입력해 주세요."
           className="me-2"
           aria-label="검색"
+          onChange={handleChange}
+          value={searchText}
         />
-        <Button variant="outline-secondary" style={{width: "100px"}}>검색</Button>
     </Form>
   );
 };
