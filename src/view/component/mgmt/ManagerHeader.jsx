10a30b9558fc6d9b5bbbc5e589ea74020a9967ed
hmshp <hyemisonne@gmail.com>
2022-04-05 20:55:10 +0900

    Design: 로고 변경, 레이아웃 어그러짐 문제 해결

--- src/view/component/mgmt/ManagerHeader.jsx
+++ src/view/component/mgmt/ManagerHeader.jsx
@@ -3,7 +3,7 @@ import { Container, Navbar, Offcanvas, Button } from 'react-bootstrap';
 import ManagerNavi from "./ManagerNavi";
 import { LinkContainer } from 'react-router-bootstrap';
 import { useNavigate } from 'react-router-dom'
-import {LOGO, H1, NAVBAR, navbarBrandStyles, BUTTON, buttonStyle} from '../../../styles/HeaderStyle';
+import {LOGO, H1, NAVBAR, navbarBrandStyles, BUTTON, MANAGERLOGOUTBTN, buttonStyle} from '../../../styles/HeaderStyle';
 import { deleteCookie, getCookie } from '../login/cookie';
 
 const ManagerHeader = () => {
@@ -44,35 +44,18 @@ const ManagerHeader = () => {
         <Navbar.Brand style={navbarBrandStyles} href="/">
           <LOGO
               alt="바로짐 로고"
-              src="https://i.ibb.co/smK8dwT/Kakao-Talk-20220308-145706090.png"
+              src="https://i.ibb.co/xD3kQPV/barogym-logo.png"
             />  
           <H1>BAROGYM</H1>
         </Navbar.Brand>
         <Navbar.Toggle/>
-        {
-          !isToggled ?
-            <>
-              <Navbar.Collapse className="justify-content-end">
-                <ManagerNavi/>
-              </Navbar.Collapse>
-            </>
-          :
-            <>
-              <Navbar.Offcanvas
-                id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel"
-                placement="end"
-              >
-                <Offcanvas.Body>
-                  <ManagerNavi/>
-                </Offcanvas.Body>
-              </Navbar.Offcanvas>
-            </>
-        }
-        <BUTTON variant="secondary" style={buttonStyle} onClick={onLogout}>로그아웃</BUTTON>
+        <Navbar.Collapse className="justify-content-end">
+          <ManagerNavi/>
+          <MANAGERLOGOUTBTN variant="secondary" style={buttonStyle} onClick={onLogout}>로그아웃</MANAGERLOGOUTBTN>
+        </Navbar.Collapse>
       </Container>
-
     </NAVBAR>
+
   );
   //로그아웃 버튼 누르고 새로고침 눌러야 쿠키 변경사항이 반영이 돼서 ManagerHeader 대신 Header가 렌더링된다
 };
