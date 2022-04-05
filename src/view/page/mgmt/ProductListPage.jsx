43af1f5aae17673d724ff4e97c395656dfc7a767
hmshp <hyemisonne@gmail.com>
2022-04-05 20:48:57 +0900

    Feat: 상품 추가 기능 구현

--- src/view/page/mgmt/ProductListPage.jsx
+++ src/view/page/mgmt/ProductListPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {PAGEHEADER,H1, STYLEDCONTAINER,STYLEDBUTTON,STYLEDSECTION,SUBSECTION} from '../../../styles/ClassStyle'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ProductList from '../../component/mgmt/ProductList';
+import { BUTTON } from '../../../styles/MyInfoStyle';
 const ProductListPage = () => {
+  const navigate = useNavigate();
   return (
     <STYLEDCONTAINER>
       <PAGEHEADER>
         <H1>상품 관리</H1>
+        <BUTTON onClick={() => navigate('/mgmt/product/insert')}>상품 추가</BUTTON>
         {/* <Link to="/mgmt/product/update"> 
           <STYLEDBUTTON >편집모드 </STYLEDBUTTON>
         </Link> */}
