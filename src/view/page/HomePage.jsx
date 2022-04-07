9d6cd7141b61188ee88c37deab378d3c987dcd36
hmshp <hyemisonne@gmail.com>
2022-04-07 17:40:08 +0900

    Design: 홈 화면 디자인 수정

--- src/view/page/HomePage.jsx
+++ src/view/page/HomePage.jsx
@@ -4,106 +4,121 @@ import Carousel from 'react-bootstrap/Carousel'
 import "./intro/style.css"
 import { Route, Link } from 'react-router-dom';
 import styled, {css} from 'styled-components';
+import KakaoMap from '../component/intro/KakaoMap';
+import { height } from '@mui/system';
+import Fade from 'react-reveal/Fade';
 
-export const STYLEDCONTAINER = styled.div`
-  margin: 0 auto;
-  @media(min-width: 800px) {
-    ${props =>
-    !props.pwCheck &&
-    css`
-      padding: 0 10em;
-    `}
-  }
-  width: 100%;
-  ${props =>
-  props.relative &&
-  css`
-    position: relative;
-  `}
-`
-
-
-
-const HomePage = () => {
+class FadeExample extends React.Component {
+  render() {
   return (
-    <STYLEDCONTAINER>
     <div style={{ textAlign: "center" }}>
-      <Carousel>
-        <Carousel.Item>
-          <img className="homeImg"
-            src="https://i.ibb.co/MndR7bt/gym-gcdd3176ab-1920.jpg"
-            alt="First slide" />
-          <Carousel.Caption>
-            <h3>BAROGYM</h3>
-            <p>송파구 최대규모의 피트니스 센터</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
 
-<div className='button'>
-  
-      <Link to="/board/notice/list/*">
-      <img  className="homeButton"
-      src="https://i.ibb.co/hYsQnBq/11.png"
-      alt="공지">
-      </img>
-      </Link>
 
-      <Link to="/board/review/list/*">
-      <img  className="homeButton"
-      src="https://i.ibb.co/q9h4y0s/444.png"
-      alt="수강후기">
-      </img>
-      </Link>
-
-      <Link to="/board/qna/list/*">
-      <img  className="homeButton"
-      src="https://i.ibb.co/FYcKjFS/33.png"
-      alt="1:1">
-   </img>
-      </Link>
-
-      <Link to="/class/appointment">
-      <img  className="homeButton"
-      src="https://i.ibb.co/yNyG0tg/22.png" 
-      alt="예약">
-      </img>
-      </Link>
-
+<div className="cropped">
+<img src="https://pix6.agoda.net/hotelImages/984/984193/984193_16091415470046449754.jpg"
+/>
 </div>
 
+
+<div className="Main">
+<Fade top>  <h2>바로짐 피트니스</h2>
+  <h1 className="a">BAROGYM <br/> FITNESS CENTER</h1>
+    <span>활력 있는 생활을 추구하는 <br/>
+     당신을 위한 맞춤 공간</span></Fade>
+</div>
+
+
+      <div className="img">        
+      <div className="homeText"> 
+        <Fade top>
+            <h1>BAROGYM</h1>
+            <h4>공지사항 및 프로모션</h4>
+            </Fade>            
+            <Link to="./board/list?id=notice&page=1"> 
+            <button className="btn">더 알아보기</button>
+            </Link>
+ </div>
+ </div>
+
+
+
+    <div className="img2">
+        <div className="content2">
+        <Link className="textLink"
+                        to="./intro/gym">   
+
+<div className="two">   
+<h1>피트니스 소개 <br/>
+  <span>Fitness Introduction</span>
+</h1>
+</div>
+</Link>
+</div>
+</div>
+
+
+<div className="img3">
+        <div className="content3">
+        <Link className="textLink"
+                        to="./intro/class">   
+<div className="two">   
+<h1>프로그램 소개<br/>
+  <span>program Introduction</span>
+</h1>
+</div>
+</Link>
+</div>
+</div>
+
+<div className="img4">
+        <div className="content4">
+        <Link className="textLink"
+                        to="./intro/teacher">   
+<div className="two">   
+<h1>트레이너 소개<br/>
+  <span>trainer Introduction</span>
+</h1>
+</div>
+</Link>
+</div>
+</div>
+
+
+<div className="img5">
+        <div className="content5">
+          <Fade top>
+             <h1>BAROGYM</h1>
+            <h4>이용권 및 가격 안내</h4>                
+          </Fade>
+          <h5> <Link to="./pass/info"> 
+            <button className="btn2" >
+        더 알아보기
+    </button> </Link> </h5> 
+        </div>
+        <div className="img-cover"></div>
+    </div>
+<br/>
 <div>
-        <Link to="./intro/gym">
-      <img  className="homeImg2"
-      src="https://i.ibb.co/JdtWhcb/image.png"
-      alt="fitness">
-      </img>
-      </Link>
-</div>
 
-<div>
-        <Link to="/intro/class">
-      <img  className="homeImg2"
-      src="https://i.ibb.co/9nmyfCM/image.png"
-      alt="GX">
-      </img>
-      </Link>
-</div>
+<div className="img6">
+        <h2 className="a">찾아오시는 길</h2>
+      
+        <h1 className="d"> 
+        <span>잠실역 도보 5분거리의<br/>
+      최다 기구 보유 대형 피트니스 센터<br/>
+      바로짐
+      </span></h1>
+      <span className="map">
+      <KakaoMap />
+      </span>
+        </div>
+        </div>
 
-<div>
-        <Link to="./intro/teacher">
-      <img  className="homeImg2"
-      src="https://i.ibb.co/HqR1SWH/image.png" 
-      alt="teacher">
-      </img>
-      </Link>
 </div>
 
 
-    </div>  
-  </STYLEDCONTAINER>
      
   );
-};
+} }
 
-export default HomePage;
+export default FadeExample;
